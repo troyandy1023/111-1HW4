769f60840e7409bddbb99014e738c356eb03b25b
Toodle <alicebobo223@gmai.com>
2022-12-19 18:50:00 +0800

    这里是 DK ，由于我们的中单选手 Showmaker（许秀）犯下关键失误，已紧急从台湾征招前职业选手 Never_LosEs（被秀），来冲击 S13 冠军。
    这里是 DK ，由于我们的打野选手 Canyon（北极熊）犯下关键失误，已紧急从台湾征招前职业选手 Never_LosEs（北七熊），来冲击 S13 冠军。
    
    这里是 DK，由于我们的下路选手 deokdam（大颗蛋）犯下关键失误，已紧急从台湾征召前职业选手 NeVeR_LosEs（小颗肾），来冲击 S13 冠军。
    
    这里是 T1，由于我们的下路选手 Gumayusi（姑妈游戏）犯下关键失误，已紧急从台湾征召前职业选手 NeVeR_LosEs（孤单油季），来冲击 S13 冠军。
    
    这里是 DK ，由于我们的上单选手 Nuguri（牛骨力）犯下关键失误，已紧急从台湾征召前王者第六 Asiagodtone（猪骨力），来冲击 S13 冠军。
    
    这里是 RGE，由于我们的辅助选手 Trymbi 犯下关键失误，已紧急从台湾征召前职业选手 NeVeR_LosEs（戳逼），来冲击 S13 冠军。
    
    这里是統一獅，由于我们的二壘手林靖凱犯下关键失误，已紧急从八德征召前電競选手 NeVeR_LosEs（熊靖凱），来冲击 2023 CPBL 冠军。

---
+++ 111-1HW4/Test.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _111_1HW4
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s_Str = ConfigurationManager.ConnectionStrings["AAA"].ConnectionString;
+            if (!IsPostBack)
+            {
+                try
+                {
+                    SqlConnection o_Str = new SqlConnection(s_Str);
+                    o_Str.Open();
+                    SqlDataAdapter o_A = new SqlDataAdapter("Select * from Users", o_Str);
+                    DataSet o_D = new DataSet();
+                    o_A.Fill(o_D, "ds_Res");//SQL轉接器物件名稱.Fill(資料集物件名稱, 資料表名稱);
+                    gd_View.DataSource = o_D;
+                    gd_View.DataBind();
+                    o_Str.Close();
+                }
+                catch (Exception o_ex)
+                {
+                    Response.Write(o_ex.ToString());
+                }
+            }
+        }
+        protected void btn_Insert_Click(object sender, EventArgs e)
+        {
+            string s_Str = ConfigurationManager.ConnectionStrings["AAA"].ConnectionString;
+            try
+            {
+                SqlConnection o_Str = new SqlConnection(s_Str);
+                o_Str.Open();
+                string str_sql = "Insert into Users (Name, Birthday)" + "values(N'阿貓阿狗', '2000/10/10');";
+                SqlCommand o_cmd = new SqlCommand(str_sql, o_Str);
+                o_cmd.ExecuteNonQuery();
+                Response.Redirect("https://localhost:44383/Test.aspx", false);
+                HttpContext.Current.ApplicationInstance.CompleteRequest();
+                o_Str.Close();
+            }
+            catch (Exception o_ex)
+            {
+                Response.Write(o_ex.ToString());
+            }
+        }
+    }
+}
